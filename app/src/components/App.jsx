6196d28f9ce8ee4810c2c4eb4c9d29ee6ac5a819
Drew Ross <mail.drew.ross@gmail.com>
2020-07-07 15:35:38 -0700

    MVP. Connect axios api call to state to render users from api.

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -11,111 +11,55 @@ const FOLLOWERS = '/followers';
 
 class App extends React.Component {
   state = {
-    userData: {
-      "login": "drew-ross",
-      "id": 62345438,
-      "node_id": "MDQ6VXNlcjYyMzQ1NDM4",
-      "avatar_url": "https://avatars2.githubusercontent.com/u/62345438?v=4",
-      "gravatar_id": "",
-      "url": "https://api.github.com/users/drew-ross",
-      "html_url": "https://github.com/drew-ross",
-      "followers_url": "https://api.github.com/users/drew-ross/followers",
-      "following_url": "https://api.github.com/users/drew-ross/following{/other_user}",
-      "gists_url": "https://api.github.com/users/drew-ross/gists{/gist_id}",
-      "starred_url": "https://api.github.com/users/drew-ross/starred{/owner}{/repo}",
-      "subscriptions_url": "https://api.github.com/users/drew-ross/subscriptions",
-      "organizations_url": "https://api.github.com/users/drew-ross/orgs",
-      "repos_url": "https://api.github.com/users/drew-ross/repos",
-      "events_url": "https://api.github.com/users/drew-ross/events{/privacy}",
-      "received_events_url": "https://api.github.com/users/drew-ross/received_events",
-      "type": "User",
-      "site_admin": false,
-      "name": "Drew Ross",
-      "company": null,
-      "blog": "",
-      "location": null,
-      "email": null,
-      "hireable": null,
-      "bio": null,
-      "twitter_username": null,
-      "public_repos": 39,
-      "public_gists": 0,
-      "followers": 4,
-      "following": 11,
-      "created_at": "2020-03-18T15:10:49Z",
-      "updated_at": "2020-07-07T19:39:19Z"
-    },
-    followersData: [
-      {
-        "login": "mrzacsmith",
-        "id": 8953340,
-        "node_id": "MDQ6VXNlcjg5NTMzNDA=",
-        "avatar_url": "https://avatars3.githubusercontent.com/u/8953340?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/mrzacsmith",
-        "html_url": "https://github.com/mrzacsmith",
-        "followers_url": "https://api.github.com/users/mrzacsmith/followers",
-        "following_url": "https://api.github.com/users/mrzacsmith/following{/other_user}",
-        "gists_url": "https://api.github.com/users/mrzacsmith/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/mrzacsmith/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/mrzacsmith/subscriptions",
-        "organizations_url": "https://api.github.com/users/mrzacsmith/orgs",
-        "repos_url": "https://api.github.com/users/mrzacsmith/repos",
-        "events_url": "https://api.github.com/users/mrzacsmith/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/mrzacsmith/received_events",
-        "type": "User",
-        "site_admin": false
-      },
-      {
-        "login": "Impulse2020",
-        "id": 23302410,
-        "node_id": "MDQ6VXNlcjIzMzAyNDEw",
-        "avatar_url": "https://avatars2.githubusercontent.com/u/23302410?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/Impulse2020",
-        "html_url": "https://github.com/Impulse2020",
-        "followers_url": "https://api.github.com/users/Impulse2020/followers",
-        "following_url": "https://api.github.com/users/Impulse2020/following{/other_user}",
-        "gists_url": "https://api.github.com/users/Impulse2020/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/Impulse2020/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/Impulse2020/subscriptions",
-        "organizations_url": "https://api.github.com/users/Impulse2020/orgs",
-        "repos_url": "https://api.github.com/users/Impulse2020/repos",
-        "events_url": "https://api.github.com/users/Impulse2020/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/Impulse2020/received_events",
-        "type": "User",
-        "site_admin": false
-      }
-    ]
+    userData: null,
+    followerData: []
   };
 
   componentDidMount() {
-    // axios.get(`${API_URL}${USER}`)
-    //   .then(res => {
-    //     this.setState({
-    //       ...this.state,
-    //       userData: res.data
-    //     });
-    //   })
-    //   .catch(err => console.log(err));
+    axios.get(`${API_URL}${USER}`)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          userData: res.data
+        });
+      })
+      .catch(err => console.log(err));
+    axios.get(`${API_URL}${USER}${FOLLOWERS}`)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          followerData: res.data
+        });
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
       <div className="App">
+        
         <header>
           <h1>Github User Card</h1>
         </header>
-        <div className="container-main">
-          <div>
-            <h2>Selected User</h2>
-            <UserCard user={this.state.userData} />
+
+        {this.state.userData
+          ?
+          <div className="container-main">
+            <div>
+              <h2>Selected User</h2>
+              <UserCard user={this.state.userData} />
+            </div>
+            <div>
+              <h2>Followers</h2>
+              <UserCardList followers={this.state.followerData} />
+            </div>
           </div>
+          :
           <div>
-            <h2>Followers</h2>
-            <UserCardList followers={this.state.followersData} />
+            <p>Getting user data...</p>
           </div>
-        </div>
+        }
+
       </div>
     );
   }
--- app/src/components/UserCard.jsx
+++ app/src/components/UserCard.jsx
@@ -6,23 +6,30 @@ class UserCard extends React.Component {
 
         const { user } = this.props;
 
-        return (
-            <div className='UserCard'>
-                <div className='top'>
-                    <div className="user-image">
-                        <a href={user.html_url}>
-                            <p className="hover-text">Github Profile &#8594;</p>
-                            <img src={user.avatar_url} alt='No avatar image found' />
-                        </a>
+        if (user) {
+            return (
+                <div className='UserCard'>
+                    <div className='top'>
+                        <div className="user-image">
+                            <a href={user.html_url} target='_blank'>
+                                <p className="hover-text">Github Profile &#8594;</p>
+                                <img src={user.avatar_url} alt='No avatar image found' />
+                            </a>
+                        </div>
+                    </div>
+                    <div className='bottom'>
+                        {user.name && <p>Name: {user.name}</p>}
+                        <p>Username: {user.login}</p>
+                        {user.public_repos && <p>Public Repos: {user.public_repos}</p>}
                     </div>
                 </div>
-                <div className='bottom'>
-                    {user.name && <p>Name: {user.name}</p>}
-                    <p>Username: {user.login}</p>
-                    {user.public_repos && <p>Public Repos: {user.public_repos}</p>}
+            );
+        } else {
+            return (
+                <div>
                 </div>
-            </div>
-        );
+            )
+        }
     }
 }
 
